cb39bdbe4848e822814e9d446004266433acee15
Muhammad Saqib <muhammadsaqiib@gmail.com>
2020-08-24 15:21:09 -0400

    Updates

---
+++ precache-manifest.f4e5cfb1128cc23c8ce110ba52173f0b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52cbeadb5ad86bb2616158d43f59fdc8",
+    "url": "/meri-rukhee/index.html"
+  },
+  {
+    "revision": "73d2c62c45bf3a85d304",
+    "url": "/meri-rukhee/static/css/main.aef6507a.chunk.css"
+  },
+  {
+    "revision": "9094eaf7434bdd5ecc09",
+    "url": "/meri-rukhee/static/js/2.0ea8b9f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meri-rukhee/static/js/2.0ea8b9f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73d2c62c45bf3a85d304",
+    "url": "/meri-rukhee/static/js/main.5587811e.chunk.js"
+  },
+  {
+    "revision": "3a06e806d5dc5de576f3",
+    "url": "/meri-rukhee/static/js/runtime-main.34ffe346.js"
+  },
+  {
+    "revision": "bf6cd02aa0637f71a86ed5917cec5ee3",
+    "url": "/meri-rukhee/static/media/bg.bf6cd02a.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meri-rukhee/static/media/logo.5d5d9eef.svg"
+  }
+]);
